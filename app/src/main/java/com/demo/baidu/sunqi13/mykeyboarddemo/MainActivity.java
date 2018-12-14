7701c75e31b1bdd29ce1dd276d248ecfbeff2a4a
sunqi <iamsshhsun@gamil.com>
2018-12-14 15:08:57 +0800

    键盘demo使用测试，待完成输入法demo

--- app/src/main/java/com/demo/baidu/sunqi13/mykeyboarddemo/MainActivity.java
+++ app/src/main/java/com/demo/baidu/sunqi13/mykeyboarddemo/MainActivity.java
@@ -1,9 +1,17 @@
 package com.demo.baidu.sunqi13.mykeyboarddemo;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText edittext1;
+    PopupKeyboardUtil smallKeyboardUtil;
+    private View viewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +29,22 @@ public class MainActivity extends AppCompatActivity {
         //冲突测试 2 from main branch
 
         //confit test2 from local_branch
+        edittext1 = (EditText) findViewById(R.id.edittext1);
+
+        smallKeyboardUtil = new PopupKeyboardUtil(self());
+        smallKeyboardUtil.attachTo(edittext1, false);
+        //smallKeyboardUtil.setAutoShowOnFocs(false);
+    }
+
+    public void onClickView(View view) {
+        if (view.getId() == R.id.btn1)
+            smallKeyboardUtil.showSoftKeyboard();
+        if (view.getId() == R.id.btn2)
+            smallKeyboardUtil.hideSoftKeyboard();
+
+    }
+
+    private Activity self() {
+        return this;
     }
 }
